4d747552ed04ead0f523b3c13a04e3aed0f6ba27
nehailemariam <35017741+nehailemariam@users.noreply.github.com>
2023-03-17 01:52:19 +0000

    Templating with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 function App() {
+
+  const name = 'StarGazers'
+  // Note the parenthesis into the multiline statement
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    // Only let's you use parent element 
+    // You can also use tag <> with no name
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="StarGazers Group" />
+          <h1>Meet the <i style={{color:"steelblue"}}>{name}</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> 
+          paving the way for peace and benevolence among all species. 
+          They are known for their enthusiasm for science, for their love of fun, 
+          and their dedication to education.</p>
+          <button className="outline" onClick={()=> alert('Hi There')}>Click Me</button>        
+        </hgroup>
+    </article>
+    </div>
+
   )
 }
 export default App
